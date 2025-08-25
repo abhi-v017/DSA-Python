9a8749809bfda56d72f4cbdd7b7993dedf79a3cf
abhishek verma <abhi.av017@gmail.com>
2025-08-25 16:24:56 +0530

    time and spacce complexity

---
+++ DSA/time_space_complexity.py
@@ -0,0 +1,9 @@
+# Time Complexity Examples
+n = int(input("Enter a number: "))
+for i in range(1, n):
+    print(i)# Time Complexity Examples
+    
+# time complexity: O(n)
+# This loop runs n times where n is the input number
+# space complexity: O(1)
+# This uses a constant amount of space
